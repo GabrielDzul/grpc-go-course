bc115f62df6faf62967a5eddaa6e36d63bfef897
Gabriel Dzul <gabrieldzul@gmail.com>
2023-10-05 10:20:39 -0600

    Added local ssl

--- greet/client/main.go
+++ greet/client/main.go
@@ -6,13 +6,27 @@ import (
 
 	pb "github.com/gabriel-dzul/grpc-go-course/greet/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true // change if necessary
+	opts := []grpc.DialOption{}
+
+	if tls {
+		cerFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(cerFile, "")
+
+		if err != nil {
+			log.Fatalf("Error while loading the cert: %s\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+
+	conn, err := grpc.Dial(addr, opts...)
 
 	if err != nil {
 		log.Fatalf("Failed to connect: %v\n", err)
@@ -25,6 +39,6 @@ func main() {
 	//doGreetManyTimes(c)
 	//doLongGreet(c)
 	//doGreetEveryone(c)
-	doGreetWithDeadline(c, 1*time.Second)
+	doGreetWithDeadline(c, 5*time.Second)
 
 }
--- greet/server/main.go
+++ greet/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	pb "github.com/gabriel-dzul/grpc-go-course/greet/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "0.0.0.0:50051"
@@ -21,9 +22,24 @@ func main() {
 		log.Fatalf("Failed to listen on: %v\n", err)
 	}
 
+	opts := []grpc.ServerOption{}
+	tls := true // Change if needed
+
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+
+		if err != nil {
+			log.Fatalf("Failed loading certificates %s\n", err)
+		}
+
+		opts = append(opts, grpc.Creds(creds))
+	}
+
 	log.Printf("Listening on %s\n", addr)
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(opts...)
 	pb.RegisterGreetServiceServer(s, &Server{})
 
 	if err = s.Serve(lis); err != nil {
